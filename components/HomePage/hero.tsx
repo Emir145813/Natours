7171900e4cfd34af22e1d67c3732f8608365f0d2
Emir145813 <www.unique1381@gmail.com>
2026-07-18 23:11:47 +0330

    Implement search bar base

---
+++ components/HomePage/hero.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Container from "../container";
+import { Button } from "../ui/button";
+
+function Hero() {
+  return (
+    <div className="bg-[url('/images/hero-page/hero-page-3.jpg')] bg-cover bg-center h-screen">
+      <div className="h-full bg-radial-[at_25%_25%] from-white/90 to-75% flex justify-center items-center">
+        <Container>
+          <div className="flex-col items-center justify-center ">
+            <h1 className="text-8xl text-center leading-[120%] font-kameron font-medium">
+              <span className="text-primary">Explore</span>
+              <br />
+              the world <br /> differently
+            </h1>
+            <div className="text-center text-lg font-medium">
+              <span className="text-primary font-semibold underline decoration-2 underline-offset-6">
+                Unique
+              </span>{" "}
+              Tours for{" "}
+              <span className="text-primary font-semibold underline decoration-2 underline-offset-6">
+                Unique
+              </span>{" "}
+              User .
+            </div>
+          </div>
+          <div className="flex items-center justify-center mt-8">
+            <Button className="px-4 py-5 rounded-full">Explore Tours</Button>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ components/HomePage/searchbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+import Container from "../container";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+function SearchBar() {
+  return (
+    <Container>
+      <div className="w-full h-40 bg-background/70 shadow-lg backdrop-blur-md rounded-4xl -mt-20 flex justify-between items-center p-8">
+        <div className="flex justify-between items-center gap-2">
+          <Icon
+            width={50}
+            icon="tabler:star"
+            className="text-primary"
+          />
+          <div className="space-y-2">
+            <p>Rating</p>
+            <Input placeholder="Search By Rate" className="bg-background" />
+          </div>
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <Icon width={50} icon="mingcute:time-duration-line" className="text-primary" />
+          <div className="space-y-2">
+            <p>Duration</p>
+            <Input
+              placeholder="How Long You Want a Break"
+              className="bg-background"
+            />
+          </div>
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <Icon width={50} icon="solar:tag-price-linear" className="text-primary" />
+          <div className="space-y-2">
+            <p>Price ?</p>
+            <Input
+              placeholder="What Is Your Price Point"
+              className="bg-background"
+            />
+          </div>
+        </div>
+        <Button className="px-4 py-6 rounded-full">Search Now</Button>
+      </div>
+    </Container>
+  );
+}
+
+export default SearchBar;
