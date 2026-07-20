24ac3c59ceb765d0563d127b5e1f66298d35fc93
Emir145813 <www.unique1381@gmail.com>
2026-07-20 23:29:13 +0330

    Updated product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -3,34 +3,40 @@ import React from "react";
 
 function ProductCard() {
   return (
-    <div className="w-60 rounded-2xl bg-card shadow-lg p-2 flex-col gap-2">
-      <div className="h-40 bg-gray-200 w-full rounded-2xl">
-        {/* Tour Image */}
-      </div>
-      <div>
-        <h1 className="text-lg text-primary font-semibold py-3">The City Wanderer</h1>
-        <div className="flex items-center gap-2">
+    <div className="h-72 w-56 bg-card rounded-xl shadow-md flex flex-col items-center justify-between p-2">
+      <div className="h-1/2 w-full rounded-xl bg-gray-200"></div>
+      <div className="w-full py-2 flex-1 flex flex-col justify-between">
+        <h1 className="text-primary font-semibold text-lg line-clamp-1">
+          The Snow Adventurer
+        </h1>
+        <div className="flex justify-start items-center">
           <div className="flex items-center gap-1">
-            <Icon
-              width={18}
-              icon="material-symbols:star-rounded"
-              className="text-accent"
-            />
-            <span className="text-sm font-medium">4.7</span>
+            <span>
+              <Icon
+                width={18}
+                icon="mingcute:time-duration-line"
+                className="text-ring"
+              />
+            </span>
+            <span className="text-lg">6</span>
           </div>
-          <Icon width={18} icon="mdi:dot" className="text-ring"/>
+          <Icon icon="bi:dot" className="text-xl text-ring" />
           <div className="flex items-center gap-1">
-            <Icon
-              width={18}
-              icon="mingcute:time-duration-line"
-              className="text-ring"
-            />
-            <span className="text-sm font-medium">6</span>
+            <span>
+              <Icon
+                width={18}
+                icon="material-symbols:star-rounded"
+                className="text-accent"
+              />
+            </span>
+            <span className="text-lg">5</span>
           </div>
         </div>
-        <div className="flex items-center gap-0.5 mt-3">
-          <Icon width={24} icon="boxicons:dollar" className="text-primary" />
-          <span className="text-2xl text-primary font-medium">1,197</span>
+        <div className="flex items-center">
+          <span>
+            <Icon width={24} icon="boxicons:dollar" className="text-primary" />
+          </span>
+          <span className="text-2xl text-primary">1200</span>
         </div>
       </div>
     </div>
